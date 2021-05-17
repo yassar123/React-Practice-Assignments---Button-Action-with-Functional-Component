b79339a5c49d63ea0cdaa4fe5775d7bbab72df16
yassar123 <yassarali1999@gmail.com>
2021-05-17 16:52:08 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import Hey from "./Hey"
 
 function App() {
+  const [click,setclick]=useState()
+  const greetUser =() => setclick("hello")
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <h1>hey</h1>
+      <button id="click" onClick={greetUser}>click</button>
+      {click === "hello"&&(<Hey/>)}
     </div>
   );
 }
---
+++ src/components/Hey.js
@@ -0,0 +1,3 @@
+import React from "react"
+const Hey=() => <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+export default Hey
